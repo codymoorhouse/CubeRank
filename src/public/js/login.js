0454fc48d1cf08392aeca5bf768729363c139b8d
Cody Moorhouse <moorhousec2@mymacewan.ca>
2016-04-03 00:59:05 -0600

    Fix incorrect password/username bug flashing on login

--- src/public/js/login.js
+++ src/public/js/login.js
@@ -3,6 +3,8 @@
     var app = angular.module('login', []);
 
     app.controller('loginCtrl', ['$window', '$scope', '$http', function ($window, $scope, $http) {
+        $scope.loginError = false;
+
         $scope.login = function (user) {
             if ($scope.loginUser.$valid) {
                 if (user === undefined ||
@@ -14,6 +16,7 @@
                 $http.post("/login", $scope.user).then(function(response) {
                     $window.location.href = '/dashboard';
                 }, function(response) {
+                    $scope.loginError = true;
                     return false;
                 });
             };
