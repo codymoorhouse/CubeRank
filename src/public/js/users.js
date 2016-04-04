e39983d474b13ae5851681a88af644e144e79d8b
Cody Moorhouse <moorhousec2@mymacewan.ca>
2016-04-04 01:44:16 -0600

    Split name on users page to first name & last name

--- src/public/js/users.js
+++ src/public/js/users.js
@@ -8,7 +8,15 @@
 
         $http.get("api/v1/users").then(function (response) {
             for (var i = 0; i < response.data.data.length; i++) {
-                $scope.userList.push(response.data.data[i]);
+                $scope.adduser = {
+                    id: response.data.data[i]['id'],
+                    fname: response.data.data[i]['name'].split(' ')[0],
+                    lname: response.data.data[i]['name'].split(' ')[1],
+                    username: response.data.data[i]['username']
+                }
+
+
+                $scope.userList.push($scope.adduser);
             }
         });
 
