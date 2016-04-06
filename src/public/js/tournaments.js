df7f496fb43ce6b690ace1cb99b3394825c7e902
John Robbins <mulvanyrobbinsj@mymacewan.ca>
2016-04-05 22:44:52 -0600

    Update tournaments view

--- src/public/js/tournaments.js
+++ src/public/js/tournaments.js
@@ -1,11 +1,18 @@
-var app = angular.module('tournaments', []);
-app.controller('tournamentController', ['$scope', '$http', '$timeout', function($scope, $http, $timeout, $window) {
-    $http.get("/api/v1/tournaments").then(function (response) {
 
+var app = angular.module('tournaments', []);
+app.controller('tournamentController', ['$scope', '$http', '$timeout', function($scope, $http, $timeout) {
+    $http.get("/api/v1/tournaments").then(function (response) {
         $scope.tournaments = response.data.data;
+
+        for (var i = 0; i < $scope.tournaments.length; i++){
+            var league_id = $scope.tournaments[i].league_id;
+            $http.get('/api/v1/leagues/'+league_id).then(function(response){
+                $scope.league_name = response.data.data.title;
+            });
+        }
         $scope.heading = 'Tournaments';
         $timeout(function () {
-            $('#users-table').dataTable();
+            $('#leagues-table').dataTable();
         }, 0);
     });
 }]);
